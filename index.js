2bd73c54adf1f8247f0f0607536e3f7e3e52c69e
Libor Svejda <Libor.Svejda@unicorn.com>
2019-10-26 19:31:55 +0200

    zohlednění principu KISS

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const createSpaServer = require("spaserver").createSpaServer;
+const apiDenVTydnu = require('./api-denvtydnu').apiDenVTydnu;
+const apiCas = require('./api-cas').apiCas;
 
 const PORT = 8080; //aplikace na Rosti.cz musi bezet na portu 8080
-
 const API_HEAD = {
     "Content-type": "application/json",
     "Access-Control-Allow-Origin": "*"
@@ -9,25 +10,14 @@ const API_HEAD = {
 const API_STATUS_OK = 0;
 const API_STATUS_NOT_FOUND = -1;
 
-const DNY_V_TYDNU = ["Neděle","Pondělí","Úterý","Středa","Čtvrtek","Pátek","Sobota"];
-
 function processApi(req, res) {
     res.writeHead(200, API_HEAD);
     let obj = {};
     obj.status = API_STATUS_OK;
     if (req.pathname === "/denvtydnu") {
-        let dt = new Date();
-        if (req.parameters.d && req.parameters.m && req.parameters.r) {
-            dt.setDate(req.parameters.d);
-            dt.setMonth(req.parameters.m-1);
-            dt.setFullYear(req.parameters.r);
-        }
-        let denVTydnu = DNY_V_TYDNU[dt.getDay()];
-        obj.datum = `${dt.getDate()}.${dt.getMonth()+1}.${dt.getFullYear()}`;
-        obj.den = denVTydnu;
+        apiDenVTydnu(req, res, obj);
     } else if (req.pathname === "/cas") {
-        let dt = new Date();
-        obj.cas = `${dt.getHours().toString().padStart(2,"0")}:${dt.getMinutes().toString().padStart(2,"0")}`;
+        apiCas(req, res, obj);
     } else {
         obj.status = API_STATUS_NOT_FOUND;
         obj.error = "API not found";
