54729afefde6d93828b0d3491b9fa23ac5ebb042
Libor Svejda <Libor.Svejda@unicorn.com>
2019-11-08 22:08:53 +0100

    přejmenování index.js na app.js, aby se nemusel editovat package.json na rosti.cz

--- api-cas.js
+++ api-cas.js
@@ -2,5 +2,5 @@ const dateFormat = require('dateformat');
 
 exports.apiCas = function (req, res, obj) {
     let dt = new Date();
-    obj.cas = dateFormat(dt, "HH.MM.ss");
+    obj.cas = dateFormat(dt, "HH:MM:ss");
 }
